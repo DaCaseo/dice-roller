55e118c52ece0218bc15588d1a63975bca03512e
BuildTools <norriscase@gmail.com>
2019-10-25 12:00:29 -0400

    added multiple dice

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::str::FromStr;
 fn main() {
     // get user input
     let mut dice_input = String::new();
-    println!("What dice would you like to roll? (<amount>d<limit>)");
+    println!("What dice would you like to roll? (<amount>d<size>)");
     io::stdin().read_line(&mut dice_input).expect("Dice is wrong");
     let dice = dice_input.trim();
     // split the user input into rolls and dice size
@@ -24,7 +24,18 @@ fn main() {
     let rolls: u16 = FromStr::from_str(split_dice[0]).unwrap();
     let size: u16 = FromStr::from_str(split_dice[1]).unwrap();
     // generate roll
+    let rolled_dice = roll_dice(rolls, size);
+    println!("{:?}", rolled_dice);
+}
+
+fn roll_dice(rolls: u16, size: u16) -> u16 {
     let mut rng = rand::thread_rng();
-    let random_roll: u16 = rng.gen_range(0, size);
-    println!("{:?}", random_roll)
+    let mut random_roll: u16 = 0;
+    let mut x: u16 = 0;
+    while x < rolls {
+        let tmp_roll: u16 = rng.gen_range(0, size);
+        random_roll += tmp_roll;
+        x += 1;
+    }
+    return random_roll;
 }
